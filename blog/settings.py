87538fa1115aaba9119499f6f0455f2793969bd9
stuguoqing <stuguoqing@126.com>
2018-06-13 16:25:46 +0800

    构建完整模板

--- blog/settings.py
+++ blog/settings.py
@@ -1,3 +1,9 @@
+#************************************
+
+#myblog是一个应用,blog是创建的项目
+
+
+#************************************
 """
 Django settings for blog project.
 
@@ -55,7 +61,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
