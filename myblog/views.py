cc1a652066d7b33237fca0bffa88a2fa350bc46a
stuguoqing <stuguoqing@126.com>
2018-06-14 11:48:32 +0800

    显示归档日期

--- myblog/views.py
+++ myblog/views.py
@@ -24,3 +24,11 @@ def detail(request,pk):
 	return render(request, 'blog/detail.html', context={'post': post})
 
 
+def archives(request, year, month):
+	post_list = Post.objects.filter(
+									created_time__year=year,
+									created_time__month=month,
+									).order_by('-created_time')
+	return render(request, 'blog/index.html',context={'post_list':post_list})
+
+
