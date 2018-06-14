b595d2c8539d691386a6384758d7c769774fb15c
stuguoqing <stuguoqing@126.com>
2018-06-14 10:03:37 +0800

    页面跳转，修改完毕

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,6 +1,8 @@
 #！！！！！！！！！！！！！！！！！！！！
 #************************************
 #警告：别眼瞎看错路径，这是myblog文件的相关应用的url配置
+#看错自个打脸，啪啪啪的打！！！！！！！！
+#*************************************
 #！！！！！！！！！！！！！！！！！！！！
 
 from django.conf.urls import url
@@ -9,6 +11,7 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-	url(r'$',views.index,name='index'),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+	url(r'$',views.index,name='index'),
+	
 ]
