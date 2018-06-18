4fd813c62075a1b0a5c004ea3999716182ece4fb
stuguoqing <stuguoqing@126.com>
2018-06-18 19:44:51 +0800

    kill textcont

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from blog.models import Post
+from myblog.models import Post
 
 # Create your views here.
 from .models import Comment
@@ -24,10 +24,10 @@ def post_comment(request,post_pk):
 						'post':post,
 						'form':form,
 						'comment_list':comment_list,
-			}
-			return render(request,'blog/detail.html',context=context)
+					}
+			return render(request, 'blog/detail.html', context=context)
 # 不是 post 请求，说明用户没有提交数据，重定向到文章详情页。
-return redirect(post)
+	return redirect(post)
 
 
 
